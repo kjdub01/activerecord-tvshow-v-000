b481f49aa08dbefb1094ceb2f56cd278ea3c9997
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-08 18:55:58 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,7 +21,7 @@ class Show < ActiveRecord::Base
   end 
   
   def Show::popular_shows
-    Show.order(rating: :desc) > 5
+    Show.where('rating' > 5)
   end 
   
   def Show::shows_by_alphabetical_order
