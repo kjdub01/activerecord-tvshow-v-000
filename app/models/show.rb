251338727cbeafcd5fab293f1b5e1ceb25ac1522
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-08 18:53:41 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def Show::most_popular_show
-    Show.order(rating: :decs).first
+    Show.order(rating: :desc).first
   end
   
   def Show::lowest_rating
